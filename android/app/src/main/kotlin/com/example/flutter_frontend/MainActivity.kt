dd045e3cc831cefae33cc832167a9d62adaf7b48
Putra28 <suyapratama2217@gmail.com>
2025-01-23 15:05:36 +0700

    "Enable KIosk Mode"

--- android/app/src/main/kotlin/com/example/flutter_frontend/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_frontend/MainActivity.kt
@@ -1,5 +1,30 @@
 package com.example.flutter_frontend
 
 import io.flutter.embedding.android.FlutterActivity
+import android.app.Activity
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.app.admin.DevicePolicyManager
+import android.os.Bundle
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Set lock task mode (Kiosk Mode)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            val policyManager = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+            val componentName = ComponentName(this, MainActivity::class.java)
+
+            if (!policyManager.isAdminActive(componentName)) {
+                val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
+                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName)
+                intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Enable Kiosk Mode")
+                startActivityForResult(intent, 1)
+            } else {
+                startLockTask()
+            }
+        }
+    }
+}
